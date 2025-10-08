938e058fb9b8d3c2ef17f3c0ef1052e0da131e1a
Gabrielle Badoch <gabi.badoch@gmail.com>
2025-10-08 16:07:42 +0200

    Mise à jour complète du projet local (fix Tailwind, Vite, etc.)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        zenblue: "#b3cde0",
+        zengreen: "#ccebc5",
+        zenpeach: "#fbb4ae",
+      },
+    },
   },
   plugins: [],
-}
+};
